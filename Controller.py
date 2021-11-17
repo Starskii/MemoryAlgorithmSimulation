52866b704a637afc459db1f1d8f964e2ab0d8a6c
Starskii <jakeylakie@yahoo.com>
2021-11-17 15:14:24 -0500

    set/get methods in Model

--- Controller.py
+++ Controller.py
@@ -3,5 +3,20 @@ import View
 
 file_name = input("Name of Input File: ")
 input_file = open(file_name, 'r')
-input_file_commands = input_file.readlines()
+input_file_lines = input_file.readlines()
+
+index = 0
+for line in input_file_lines:
+    commands = line.split(' ')
+    if index == 0:
+        pass
+    elif index == 1:
+        pass
+    elif commands[0] == 'request':
+        pass
+    elif commands[0] == 'release':
+        pass
+    else:
+        print("Invalid Operation\n")
+    index += 1
 
--- Model.py
+++ Model.py
@@ -6,3 +6,26 @@ class Process:
 class Resource:
     def __init__(self, max_r):
         self.max = max_r
+        self.current = max_r
+
+
+resources = []
+processes = []
+
+
+def add_resource(max_r):
+    res = Resource(max_r)
+    resources.append(res)
+
+
+def add_process(num):
+    for x in range(0, num):
+        processes.append(Process())
+
+
+def get_processes():
+    return processes
+
+
+def get_resources():
+    return resources
